906ec3873fe940c8b513b829f2927e14eea30ff7
jshsltr <35411248+jshsltr@users.noreply.github.com>
2023-12-16 23:49:17 -0600

    Update esp32_midea_RS485.h

--- src/esp32_midea_RS485.h
+++ src/esp32_midea_RS485.h
@@ -69,9 +69,9 @@
 
 class ESP32_Midea_RS485Class {
   public:
-    ESP32_Midea_RS485Class(HardwareSerial *hwSerial, uint8_t ro_pin, uint8_t di_pin, uint8_t re_de_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
+    ESP32_Midea_RS485Class(HardwareSerial *hwSerial, uint8_t ro_pin, uint8_t di_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
     
-    virtual void begin(HardwareSerial *hwSerial, uint8_t ro_pin, uint8_t di_pin, uint8_t re_de_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
+    virtual void begin(HardwareSerial *hwSerial, uint8_t ro_pin, uint8_t di_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
     uint8_t SetMode(MideaACOpModeType mode);
     uint8_t SetFanMode(MideaACFanModeType fan_mode);
     uint8_t SetTemp(uint8_t temp);
@@ -86,7 +86,6 @@ class ESP32_Midea_RS485Class {
     uint8_t SentData[16];
     uint8_t ReceivedData[40];
   private:
-    uint8_t ComControlPin;
     HardwareSerial *SerialBus;
     uint8_t datain_pin;
     uint8_t dataout_pin;
